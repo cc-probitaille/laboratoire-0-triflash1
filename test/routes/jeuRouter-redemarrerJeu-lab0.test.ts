70804dca19965928e8bf2622298dcee54c83570c
TriFl4sh1 <triflash1.clouthier@gmail.com>
2025-09-09 15:11:07 -0400

    Numéro 3 Fini (README)

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,28 @@
 // Vous devez insérer les nouveaux tests ici
-import { assert } from 'console';
+import supertest from 'supertest';
+import app from '../../src/app';
+import { jeuRoutes } from "../../src/routes/jeuRouter";
 import 'jest-extended';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
-  });
+const request = supertest(app);
+
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
 });
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  it("Devrais pouvoir relancer le jeu sans joueurs", async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu/');
+    expect(response.status).toBe(200);
+
+  })
+
+  it('devrait rendre 0 pour le nombre de Joueurs', async () => {
+      const joueursJSON = jeuRoutes.controleurJeu.joueurs;
+      const joueursArray = JSON.parse(joueursJSON);
+      expect(joueursArray.length).toBe(0);
+    });
+})
